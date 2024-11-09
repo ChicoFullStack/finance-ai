c9f9dff2c3f73cb64c47e4ada33d03e2c01b9410
chicofullstack <chico_fullstack_2023@outlook.com>
2024-11-08 22:38:58 -0300

    feat: aad summary cards

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,15 +3,23 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  // Buscar transações do usuário
+  const transactions = await db.transaction.findMany({
+    where: { userId },
+  });
   return (
     <div>
       <Navbar />
 
       <div className="space-y-6 p-6">
-        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
